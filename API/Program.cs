e4103a377d71d0b36033f4501bd957f564ae6663
Shaunakjs <shaunakjoshi1995@gmail.com>
2024-03-20 16:46:49 +0530

    Error handling

---
+++ API/Program.cs
@@ -0,0 +1,53 @@
+using API.Errors;
+using API.Extensions;
+using API.Middleware;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApplicationServices(builder.Configuration);
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+// Configure the HTTP request pipeline.
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseStaticFiles();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+var context = services.GetRequiredService<StoreContext>();
+var logger = services.GetRequiredService<ILogger<Program>>();
+try
+{
+    await context.Database.MigrateAsync();
+    await StoreContextSeed.SeedAsync(context);  
+}
+
+catch(Exception ex)
+{
+    logger.LogError(ex, "An error occured during migration");
+}
+
+app.Run();
